5378d5b78bd30508453bab80e7b392cb66984cff
LeopoldHsing <LeopoldHsing@gmail.com>
2024-09-03 08:26:39 -0500

    first commit

---
+++ FirstFile.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+puts "你好，世界！";
+
+# string
+opening_line = 'It was the best of time'
+movie_trailer = "In a land that time forgot"
+opening_line += "It was the blurst of times"
+puts opening_line
