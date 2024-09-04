dbe949658d2256941fd31feabbbf128a4b0978fd
LeopoldHsing <LeopoldHsing@gmail.com>
2024-09-04 15:04:42 -0500

    assignment 1 revised

--- Module1/assignment/2. Updated Tax Calculate Program.rb
+++ Module1/assignment/2. Updated Tax Calculate Program.rb
@@ -5,17 +5,17 @@ Course: Agile Full Stack Web Development
 
 sub_total = 5.00
 
-GST_RATE = 0.05
-PST_RATE = 0.07
+GOODS_AND_SERVICES_TAX_RATE = 0.05
+PROVINCIAL_SALES_TAX_RATE = 0.07
 
-gst_amount = sub_total * GST_RATE
-pst_amount = sub_total * PST_RATE
+gst_amount = sub_total * GOODS_AND_SERVICES_TAX_RATE
+pst_amount = sub_total * PROVINCIAL_SALES_TAX_RATE
 
 grand_total = sub_total + gst_amount + pst_amount
 
 puts "Subtotal: $#{sub_total}"
-puts "PST: $#{pst_amount.round(2)} - #{(PST_RATE * 100).to_i}%"
-puts "GST: $#{gst_amount.round(2)} - #{(GST_RATE * 100).to_i}%"
+puts "PST: $#{pst_amount.round(2)} - #{(PROVINCIAL_SALES_TAX_RATE * 100).to_i}%"
+puts "GST: $#{gst_amount.round(2)} - #{(GOODS_AND_SERVICES_TAX_RATE * 100).to_i}%"
 puts "Grand Total: $#{grand_total.round(2)}"
 
 if grand_total <= 5.00
--- Module1/assignment/3. Tax Calculator with Input.rb
+++ Module1/assignment/3. Tax Calculator with Input.rb
@@ -4,21 +4,27 @@ Course: Agile Full Stack Web Development
 =end
 
 print "Please enter the subtotal: $"
+# get user input
 sub_total = gets.chomp.to_f
 
-GST_RATE = 0.05
-PST_RATE = 0.07
+# set tax rate value
+GOODS_AND_SERVICES_TAX_RATE = 0.05
+PROVINCIAL_SALES_TAX_RATE = 0.07
 
-gst_amount = sub_total * GST_RATE
-pst_amount = sub_total * PST_RATE
+# calculate the tax
+gst_amount = sub_total * GOODS_AND_SERVICES_TAX_RATE
+pst_amount = sub_total * PROVINCIAL_SALES_TAX_RATE
 
+# calculate grand total amount
 grand_total = sub_total + gst_amount + pst_amount
 
+# print out the result
 puts "Subtotal: $#{sub_total.round(2)}"
-puts "PST: $#{pst_amount.round(2)} - #{(PST_RATE * 100).to_i}%"
-puts "GST: $#{gst_amount.round(2)} - #{(GST_RATE * 100).to_i}%"
+puts "PST: $#{pst_amount.round(2)} - #{(PROVINCIAL_SALES_TAX_RATE * 100).to_i}%"
+puts "GST: $#{gst_amount.round(2)} - #{(GOODS_AND_SERVICES_TAX_RATE * 100).to_i}%"
 puts "Grand Total: $#{grand_total.round(2)}"
 
+# condition determination
 if grand_total <= 5.00
   puts "Pocket Change"
 elsif grand_total < 20.00
