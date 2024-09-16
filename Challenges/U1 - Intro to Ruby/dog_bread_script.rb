917153eea2fa40dbb6e4c2459bb6c17d24f68ba6
LeopoldHsing <LeopoldHsing@gmail.com>
2024-09-15 19:47:29 -0500

    add comments

--- Challenges/U1 - Intro to Ruby/dog_bread_script.rb
+++ Challenges/U1 - Intro to Ruby/dog_bread_script.rb
@@ -9,9 +9,12 @@ dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
 pp dog_breeds # pp stands for pretty print.
 
 dog_breeds['message'].each do |breed, sub_breeds|
+  # iterate all breads
   if sub_breeds.empty?
+    # when there is no sub bread
     puts "* #{breed.capitalize}"
   else
+    # when there are sub breads
     puts "* #{breed.capitalize}"
     sub_breeds.each { |sub_breed| puts "  * #{sub_breed.capitalize}" }
   end
