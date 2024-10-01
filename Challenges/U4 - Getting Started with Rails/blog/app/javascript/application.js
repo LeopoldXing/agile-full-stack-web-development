23a054e496ac54bc330e0875f32801acf65cdd7c
LeopoldHsing <LeopoldHsing@gmail.com>
2024-09-30 22:08:42 -0500

    U4 challenge Getting Started with Rails

---
+++ Challenges/U4 - Getting Started with Rails/blog/app/javascript/application.js
@@ -0,0 +1,3 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "Challenges/U4 - Getting Started with Rails/blog/app/javascript/controllers"
